febb558f5a3e3f546c08960fc56c87b5f3472932
novb1492 <novb1492@naver.com>
2021-04-20 21:53:02 +0900

    목표작성프로세스수정

--- processes/dreamsprocess.php
+++ processes/dreamsprocess.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require("../lib/lib.php");
+require("../lib/lib2.php");
 
 $email=$_POST['email'];
 $dreamtitle=$_POST['dreamtitle'];
@@ -10,44 +10,32 @@ $period=$_POST['period'];
 $situation=$_POST['situation'];
 $month=date("n");
 $day=date("j");
-
+echo $day;
 if($situation=="insert")
 {
-    $sql2="INSERT INTO dreams(id,period,email,dreamtitle,dreamdescription,dreamcreate,months,days) VALUES('$id','$period','$email','$dreamtitle','$dreamdescription',now(),$month,$day)";
+    $sql="INSERT INTO dreams(id,period,email,dreamtitle,dreamdescription,dreamcreate,months,days) VALUES('$id','$period','$email','$dreamtitle','$dreamdescription',now(),'$month','$day')";
     $situation="insert";
+    $successtext="목표등록에 성공";
 }
 else if($situation=="update")
 {
-    $sql2= "UPDATE dreams SET dreamtitle='$dreamtitle',dreamdescription='$dreamdescription',period='$period',dreamcreate=now(),months='$month',days='$day' WHERE email='$email'";
+    $sql= "UPDATE dreams SET dreamtitle='$dreamtitle',dreamdescription='$dreamdescription',period='$period',dreamcreate=now(),months='$month',days='$day' WHERE email='$email'";
     $situation="update";   
-}
-    $result = process($sql2,$situation);
+    $successtext="목표변경에 성공";
    
-    $bool=check($result);////저장확인
+}
+else
+{
+    $failtext="목표지정에 실패했습니다.";
+}
 
-    if($bool==1)
-    {
-    ?>
-    <script>
-        var text="목표를 설정했습니다."
-        dingdong(text);
-    </script>
-    <?php
-    }   
-    else 
-    {
-    ?>
-    <script>
-        var text="목표설정에 실패 했습니다."
-        dingdong(text);
-    </script>
-    <?php
-    }
+$bool=process($sql,$situation);
 
-    $situation="select";
-    $sql2="SELECT dreamcreate FROM dreams WHERE email ='$email'";////////dreams테이블에서 변경된 시간 가져오기
-    $row=process($sql2,$situation);
-    $dreamcreate=$row['dreamcreate'];
-
-    sessiondream($dreamtitle,$dreamdescription,$period,$dreamcreate,$month,$day);   
+if($bool==true)
+{
+    $now = date('Y-m-d H:i:s');
+    sessiondream($dreamtitle,$dreamdescription,$period,$now,$month,$day);
+}
+$link='../pages/dream.php';
+check($bool,$successtext,$failtext,$link);
 ?>
