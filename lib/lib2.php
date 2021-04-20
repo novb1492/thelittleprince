419cda93bd0b3a134f4cc8a90e7a9507192f8e7b
novb1492 <novb1492@naver.com>
2021-04-20 15:26:52 +0900

    새로운 lib러리 구축중

--- lib/lib2.php
+++ lib/lib2.php
@@ -1,4 +1,20 @@
 <?php
+    function makeinsert($table,$calums,$values)
+    {
+        return "INSERT INTO $table($calums) VALUES('.$values[0].','$pwd',now(),'$phone1','$phone2','$phone3')";
+    }
+    function mysqlconnect()///////////////데이터베이스에 들어갈 암호및 경로
+    {
+        return mysqli_connect("localhost","root", "6937544","thelittleprince");
+    }
+    function inputquery($conn,$sql)///////////////sql문 대입하기
+    {
+        return mysqli_query($conn, $sql);
+    }
+    function fencharray($result)///////////////////////s1l문내용대로 가져오기
+    {
+        return mysqli_fetch_array($result);
+    }
     function savecheck($result)
     {
         if ($result === false) 
@@ -28,21 +44,4 @@
         </script>
         <?php
     }
-
-?>
-
-<?php
-function mysqlconnect()///////////////데이터베이스에 들어갈 암호및 경로
-{
-    return mysqli_connect("localhost","root", "6937544","thelittleprince");
-}
-function inputquery($conn,$sql)///////////////sql문 대입하기
-{
- 
-    return mysqli_query($conn, $sql);
-}
-function fench($result)///////////////////////s1l문내용대로 가져오기
-{
-    return mysqli_fetch_array($result);
-}
 ?>
--- processes/singupprocess.php
+++ processes/singupprocess.php
@@ -7,9 +7,32 @@ $phone1=$_POST['phone1'];
 $phone2=$_POST['phone2'];
 $phone3=$_POST['phone3'];
 
-$sql="INSERT INTO user(email,password,created,phone1,phone2,phone3) VALUES('$email','$pwd',now(),'$phone1','$phone2','$phone3')";
+$sql="SELECT email FROM user WHERE email='$email'";
 
 $conn=mysqlconnect();///////////데이터베이스 접속
+
+$result=inputquery($conn, $sql);//////////sql문 대입하기 ->결과가져오기
+
+$row=fencharray($result);
+
+
+if($email==$row['email'])
+{
+    $bool=false;
+    $failtext="중복된 아이디 입니다.";
+}
+else
+{
+    
+/*$table='user';
+$calums='email,password,created,phone1,phone2,phone3';
+$values=array($email,$pwd,$phone1,$phone2,$phone3);
+$sql="INSERT INTO $user(email,password,created,phone1,phone2,phone3) VALUES('$email','$pwd',now(),'$phone1','$phone2','$phone3')";
+$sql="INSERT INTO $table($calums) VALUES('$values[0]','$pwd',now(),'$phone1','$phone2','$phone3')";
+이렇게도 가능했다 하지만 values값에 각기다른 배열을 반복문으로 줘야해서 생각을 많이해봐야갰다 2021-4-20
+*/
+
+$sql="INSERT INTO user(email,password,created,phone1,phone2,phone3) VALUES('$email','$pwd',now(),'$phone1','$phone2','$phone3')";
 
 //mysqli_select_db($conn,"thelittleprince");//////데이터베이스 선택
 
@@ -20,7 +43,6 @@ $bool=savecheck($result);////저장확인
 $successtext="회원가입 성공";
 $failtext="회원가입 실패";
 
+}
 check($bool,$successtext,$failtext);
-
-
 ?>
