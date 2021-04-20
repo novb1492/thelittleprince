43391ac029dcc65d9773f67b941ad1fab81f351f
novb1492 <novb1492@naver.com>
2021-04-20 15:29:49 +0900

    lib러리수정중

--- processes/singupprocess.php
+++ processes/singupprocess.php
@@ -7,14 +7,15 @@ $phone1=$_POST['phone1'];
 $phone2=$_POST['phone2'];
 $phone3=$_POST['phone3'];
 
-$sql="SELECT email FROM user WHERE email='$email'";
+$table='user';
+
+$sql="SELECT email FROM $table WHERE email='$email'";
 
 $conn=mysqlconnect();///////////데이터베이스 접속
 
 $result=inputquery($conn, $sql);//////////sql문 대입하기 ->결과가져오기
 
 $row=fencharray($result);
-
 
 if($email==$row['email'])
 {
@@ -23,7 +24,7 @@ if($email==$row['email'])
 }
 else
 {
-    
+
 /*$table='user';
 $calums='email,password,created,phone1,phone2,phone3';
 $values=array($email,$pwd,$phone1,$phone2,$phone3);
@@ -32,7 +33,7 @@ $sql="INSERT INTO $table($calums) VALUES('$values[0]','$pwd',now(),'$phone1','$p
 이렇게도 가능했다 하지만 values값에 각기다른 배열을 반복문으로 줘야해서 생각을 많이해봐야갰다 2021-4-20
 */
 
-$sql="INSERT INTO user(email,password,created,phone1,phone2,phone3) VALUES('$email','$pwd',now(),'$phone1','$phone2','$phone3')";
+$sql="INSERT INTO $table(email,password,created,phone1,phone2,phone3) VALUES('$email','$pwd',now(),'$phone1','$phone2','$phone3')";
 
 //mysqli_select_db($conn,"thelittleprince");//////데이터베이스 선택
 
